e323e6306b63f1fad29f5603efc9ac8e74c13eeb
Alexander Skovpen <a.n.skovpen@gmail.com>
2015-05-11 01:21:16 +0300

    urlgenerator changed

--- controller/pagecontroller.php
+++ controller/pagecontroller.php
@@ -43,7 +43,7 @@ class PageController extends Controller {
 	 * @NoCSRFRequired
 	 */
 	public function index() {
-    $widgeturl=$this->urlgenerator->getAbsoluteURL(\OC::$server->getURLGenerator()->linkToRoute('treebookmarks.page.widget'));
+    $widgeturl=$this->urlgenerator->linkToRoute('treebookmarks.page.widget');
 		$params = ['user' => $this->userId, 'url'=>$widgeturl];
 		return new TemplateResponse('treebookmarks', 'main', $params);  // templates/main.php
 	}
